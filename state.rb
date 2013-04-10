183ee58aef46e91b4742373ba985bc0e2a1e8e6c
Jamie B <jamieb28@gmail.com>
2013-04-10 18:29:10 -0300

    Update state.rb

--- state.rb
+++ state.rb
@@ -1,1 +1,16 @@
 class State
+  def initialize(name, abbreviation)
+		@name = name
+		@abbreviation = abbreviation		
+	end
+
+	def name(abbreviation)
+		@abbreviation = abbreviation
+		put @name
+	end
+
+	def abbreviation(name)
+		@name = name
+		put @abbreviation
+	end
+endclass State
