22a3b3fa0146a2a65b36c6c99daac941ccf969df
Jamie Burns <jamieb28@gmail.com>
2013-04-15 19:46:55 -0400

    fixing it again

--- state.rb
+++ state.rb
@@ -1,16 +1,21 @@
 class State
-  def initialize(name, abbreviation)
-		@name = name
-		@abbreviation = abbreviation		
+
+	STATES = {"AL"=>"Alabama", "AK"=>"Alaska", "AZ"=>"Arizona", "AR"=>"Arkansas", "CA"=>"California", "CO"=>"Colorado", "CT"=>"Connecticut", "DE"=>"Delaware", "FL"=>"Florida", "GA"=>"Georgia", "HI"=>"Hawaii", "ID"=>"Idaho", "IL"=>"Illinois", "IN"=>"Indiana", "IA"=>"Iowa", "KS"=>"Kansas", "KY"=>"Kentucky", "LA"=>"Louisiana", "ME"=>"Maine", "MD"=>"Maryland", "MA"=>"Massachusetts", "MI"=>"Michigan", "MN"=>"Minnesota", "MS"=>"Mississippi", "MO"=>"Missouri", "MT"=>"Montana", "NE"=>"Nebraska", "NV"=>"Nevada", "NH"=>"New Hampshire", "NJ"=>"New Jersey", "NM"=>"New Mexico", "NY"=>"New York", "NC"=>"North Carolina", "ND"=>"North Dakota", "OH"=>"Ohio", "OK"=>"Oklahoma", "OR"=>"Oregon", "PA"=>"Pennsylvania", "RI"=>"Rhode Island", "SC"=>"South Carolina", "SD"=>"South Dakota", "TN"=>"Tennessee", "TX"=>"Texas", "UT"=>"Utah", "VT"=>"Vermont", "VA"=>"Virginia", "WA"=>"Washington", "WV"=>"West Virginia", "WI"=>"Wisconsin", "WY"=>"Wyoming"}
+
+	def name(key)
+		STATES[key.upcase]
+	end	
+
+	def abbr(key)
+		STATES.invert[key]
 	end
 
-	def name(abbreviation)
-		@abbreviation = abbreviation
-		put @name
+	def all
+		STATES.values.sort
 	end
 
-	def abbreviation(name)
-		@name = name
-		put @abbreviation
+	def all_abbr
+		STATES.keys.sort
 	end
-endclass State
+
+end
